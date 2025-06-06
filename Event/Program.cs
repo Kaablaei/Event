1ebfaf79a60ac96dfe9ab01699ac06c23b7b5d5c
AmirHossein Biabani <amirhosseinbyabani@yahoo.com>
2025-06-06 21:19:28 +0330

    Add project files.

---
+++ Event/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+
+Player p = new Player();
+
+p.AddPoint();
+p.AddPoint();
+p.AddPoint();
+p.AddPoint();
+p.AddPoint();
+
+
+
+
+
+public class Player
+{
+    public int Level { get; set; }
+
+
+
+    public void AddPoint()
+    {
+        Level += 10;
+        Console.WriteLine($"You Get 10 Point ,Your Level is {Level}");
+
+        if(Level >= 50)
+        {
+            Console.WriteLine("50");
+        }
+    }
+
+
+}
