6ee2edbce75496d946e4d04a75e43fb98054a658
AmirHossein Biabani <amirhosseinbyabani@yahoo.com>
2025-06-06 21:29:08 +0330

    After Using Events

--- Event/Program.cs
+++ Event/Program.cs
@@ -3,12 +3,19 @@
 
 
 Player p = new Player();
+p.OnAchlievementUnlocked += P_OnAchlievementUnlocked;
+
+void P_OnAchlievementUnlocked()
+{
+    Console.WriteLine("50");
+}
 
 p.AddPoint();
 p.AddPoint();
 p.AddPoint();
 p.AddPoint();
 p.AddPoint();
+
 
 
 
@@ -18,6 +25,11 @@ public class Player
 {
     public int Level { get; set; }
 
+
+    public delegate void AchlievementUnlocked();
+
+    public event AchlievementUnlocked? OnAchlievementUnlocked;
+
     public void AddPoint()
     {
         Level += 10;
@@ -25,7 +37,8 @@ public class Player
 
         if(Level >= 50)
         {
-            Console.WriteLine("50");
+            //Console.WriteLine("50");
+            OnAchlievementUnlocked?.Invoke();
         }
     }
 
