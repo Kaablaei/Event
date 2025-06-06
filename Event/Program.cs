0854933cb9a0fb063d2cf6ba579cc37f8f1000b5
AmirHossein Biabani <amirhosseinbyabani@yahoo.com>
2025-06-06 21:43:39 +0330

    Using  LevelGangeEventArgs

--- Event/Program.cs
+++ Event/Program.cs
@@ -3,9 +3,17 @@
 
 
 using System.Drawing;
+using System.Reflection.Emit;
 
 Player p = new Player();
 p.OnAchlievementUnlocked += P_OnAchlievementUnlocked;
+
+p.OnLevelChangeHandler += P_OnLevelChangeHandler;
+
+void P_OnLevelChangeHandler(object sender, LevelGangeEventArgs e)
+{
+    Console.WriteLine($"You Get {e.NewPoint} Point ,Your Level is {e.TotalPoint}");
+}
 
 void P_OnAchlievementUnlocked(int Point)
 {
@@ -32,12 +40,18 @@ public class Player
 
     public event AchlievementUnlocked? OnAchlievementUnlocked;
 
+
+    public delegate void LevelChangeHandler(object sender, LevelGangeEventArgs e);
+    public event LevelChangeHandler? OnLevelChangeHandler;
+
+
     public void AddPoint()
     {
         Level += 10;
-        Console.WriteLine($"You Get 10 Point ,Your Level is {Level}");
+        //Console.WriteLine($"You Get 10 Point ,Your Level is {Level}");
+        OnLevelChangeHandler?.Invoke(this,new LevelGangeEventArgs(10,Level));
 
-        if(Level >= 50)
+        if (Level >= 50)
         {
             //Console.WriteLine("50");
             OnAchlievementUnlocked?.Invoke(Level);
@@ -45,4 +59,17 @@ public class Player
     }
 
 
+}
+
+
+public class LevelGangeEventArgs : EventArgs
+{
+    public int NewPoint {  get;  }
+    public int TotalPoint { get;  }
+
+    public LevelGangeEventArgs(int newpoint, int totalpoint)
+    {
+        NewPoint = newpoint;
+        TotalPoint = totalpoint;
+    }
 }
